cbce08c265302d15e92e5ad0ad3265705a9ec022
ShivamGo <ssukggo@gmail.com>
2018-01-03 15:35:11 +0530

    first commmit.

--- string/Solution.java
+++ string/Solution.java
@@ -4,4 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello github ...");
+    }
 }
